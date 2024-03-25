1fbdfa58891eab89f271c81729a32ce9995afe20
basitmir2020 <xaid902@gmail.com>
2024-03-25 21:40:31 +0530

    Commit : Some Changes in Index

--- src/shared/dashboard/dashboard.component.ts
+++ src/shared/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {NgOptimizedImage, TitleCasePipe} from "@angular/common";
 import {filter} from "rxjs";
+import PerfectScrollbar from "perfect-scrollbar";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,14 +12,28 @@ import {filter} from "rxjs";
     RouterLink,
     NgOptimizedImage,
     TitleCasePipe,
-    RouterLinkActive
+    RouterLinkActive,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit,AfterViewInit {
   pageTitle:string = '';
-  constructor(private route: ActivatedRoute,private router: Router) { }
+
+  iconNavbarSidenav = document.getElementById('iconNavbarSidenav');
+  iconSidenav = document.getElementById('iconSidenav');
+  sidenav = document.getElementById('sidenav-main');
+  body = document.getElementsByTagName('body')[0];
+  className = 'g-sidenav-pinned';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private elementRef: ElementRef) { }
+
+  ngAfterViewInit(): void {
+    this.initializePerfectScrollbar();
+  }
 
   ngOnInit(): void {
     this.router.events
@@ -32,5 +47,42 @@ export class DashboardComponent implements OnInit {
         }
         this.pageTitle = lastSegment;
       });
+  }
+
+  private initializePerfectScrollbar(): void {
+    const mainpanel = this.elementRef.nativeElement.querySelector('.main-content');
+    const sidebar = this.elementRef.nativeElement.querySelector('.sidenav');
+    const fixedplugin = this.elementRef.nativeElement.querySelector('.fixed-plugin');
+    const navbarCollapse = this.elementRef.nativeElement.querySelector('.navbar-collapse');
+
+    let ps = new PerfectScrollbar(mainpanel);
+    let ps1 = new PerfectScrollbar(sidebar);
+    let ps2 = new PerfectScrollbar(fixedplugin);
+    let ps3 = new PerfectScrollbar(fixedplugin);
+
+
+    if (this.iconNavbarSidenav) {
+      this.iconNavbarSidenav.addEventListener("click", this.toggleSidenav);
+    }
+
+    if (this.iconSidenav) {
+      this.iconSidenav.addEventListener("click", this.toggleSidenav);
+    }
+  }
+
+  private toggleSidenav() {
+    if (this.body.classList.contains(this.className)) {
+      this.body.classList.remove(this.className);
+      setTimeout(()=>{
+        this.sidenav?.classList.remove('bg-white');
+      }, 100);
+      this.sidenav?.classList.remove('bg-transparent');
+
+    } else {
+      this.body.classList.add(this.className);
+      this.sidenav?.classList.add('bg-white');
+      this.sidenav?.classList.remove('bg-transparent');
+      this.iconSidenav?.classList.remove('d-none');
+    }
   }
 }
