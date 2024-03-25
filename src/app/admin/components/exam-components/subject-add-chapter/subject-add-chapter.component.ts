787be3c8c580590eb2045ff960a76623e00b0602
basitmir2020 <xaid902@gmail.com>
2024-03-25 15:54:10 +0530

    Commit : UI changes done for Question

--- src/app/admin/components/exam-components/subject-add-chapter/subject-add-chapter.component.ts
+++ src/app/admin/components/exam-components/subject-add-chapter/subject-add-chapter.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgxSpinnerModule, NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-subject-add-chapter',
   standalone: true,
-  imports: [],
+    imports: [
+        NgxSpinnerModule
+    ],
   templateUrl: './subject-add-chapter.component.html',
   styleUrl: './subject-add-chapter.component.css'
 })
-export class SubjectAddChapterComponent {
+export class SubjectAddChapterComponent implements OnInit{
+  constructor(private _spinnerService:NgxSpinnerService) {}
 
+  ngOnInit(): void {
+    this._spinnerService.show().then(r => {});
+
+    setTimeout(()=>{
+      this._spinnerService.hide().then(r => {});
+    },1000);
+  }
 }
--- src/app/admin/components/index/index.component.ts
+++ src/app/admin/components/index/index.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgxSpinnerModule, NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [
+    NgxSpinnerModule
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit{
+  constructor(private _spinnerService:NgxSpinnerService) {}
 
+  ngOnInit(): void {
+    this._spinnerService.show().then(r => {});
+
+    setTimeout(()=>{
+      this._spinnerService.hide().then(r => {});
+    },1000);
+  }
 }
