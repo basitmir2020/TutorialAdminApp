a4179e8a6c7a74ec391b11dd61ba5caac684a64d
basitmir2020 <xaid902@gmail.com>
2024-05-24 20:57:57 +0530

    Commit : Basic Changes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {ForgotPasswordComponent} from "./forgot-password/forgot-password.component";
+import {OtpVerificationComponent} from "./otp-verification/otp-verification.component";
 
 const routes: Routes = [
   { path:'',component:LoginComponent },
   {path:"forgotPassword",component:ForgotPasswordComponent},
+  {path:"otp-verification",component:OtpVerificationComponent},
   {path:"login",redirectTo:'',pathMatch:"full"}
 ];
 
